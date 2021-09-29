8e19a678b62f1160266d385285fe47534fea1a38
matteodelorenzi <88620435+sanserif-collectif@users.noreply.github.com>
2021-09-29 20:23:59 +0200

    splide broken

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,13 +1,13 @@
-import Splide from '@splidejs/splide';
+import Splide from '@splidejs/splide'
 
-new Splide( '.splide', {
+new Splide('.splide', {
     isNavigation: true,
     cover: true,
     fixedWidth: '40vw',
-    arrows: false,
+    arrows: true,
     pagination: false,
-    drag: false,
+    drag: true,
     easing: 'cubic-bezier(0, 0.55, 0.45, 1)',
     speed: 400,
     rewind: true
-}).mount();
+}).mount()
