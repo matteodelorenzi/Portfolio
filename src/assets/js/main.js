75a65b5e3bc2fae621f2300b0c9baa0ddcd3188d
matteodelorenzi <56349353+matteodelorenzi@users.noreply.github.com>
2021-11-03 15:11:41 +0100

    Swiper init

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,13 +1,49 @@
-import Splide from '@splidejs/splide'
+// import Splide from '@splidejs/splide'
 
-new Splide('.splide', {
-    isNavigation: true,
-    cover: true,
-    fixedWidth: '40vw',
-    arrows: true,
-    pagination: false,
-    drag: true,
-    easing: 'cubic-bezier(0, 0.55, 0.45, 1)',
-    speed: 400,
-    rewind: true
-}).mount()
+// new Splide('.splide', {
+//     isNavigation: true,
+//     cover: true,
+//     fixedWidth: '40vw',
+//     arrows: true,
+//     pagination: false,
+//     drag: true,
+//     easing: 'cubic-bezier(0, 0.55, 0.45, 1)',
+//     speed: 400,
+//     rewind: true
+// }).mount()
+
+import Swiper, { Navigation, Pagination, Keyboard, Mousewheel, EffectCoverflow } from 'swiper'
+import 'swiper/swiper.scss'
+
+//configure Swiper to use modules
+Swiper.use([Navigation, Pagination, Keyboard, Mousewheel, EffectCoverflow])
+
+//init Swiper
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    effect: "coverflow",
+    slidesPerView: 2.5,
+    spaceBetween: 40,
+    mousewheel: true,
+    centeredSlides: false,
+    keyboard: {
+        enabled: true,
+    },
+    loop: true,
+    coverflowEffect: {
+        rotate: 10,
+        stretch: 0,
+        depth: 50,
+        modifier: 1,
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true,
+        hide: false,
+    }
+})
+
+swiper.update()
+swiper.slideNext()
